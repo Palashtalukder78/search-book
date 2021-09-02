a061625ed7588580d0fdc24d4501b739760cb49f
Palashtalukder78 <palashtalukder78@gmail.com>
2021-09-02 10:10:18 +0600

    Completed all requerments with spinner

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,60 @@
+// Clickable Function for get data from the API
 const searchButton = () => {
+    //Make Empty
     const bookContainer = document.getElementById('books-container');
     bookContainer.textContent = '';
     const mainContainer = document.getElementById('search-result-found');
     mainContainer.textContent = '';
-    
+    //Get input field text
     const inputField = document.getElementById('input-field');
     const inputText = inputField.value;
     inputField.value = '';
-
+    //Validation : when we input Nothing
     if(inputText === ''){
         handleValidation('search-result-found','none');
         handleValidation('no-result-found','none');
         handleValidation('empty-search-field','block');
-    }else{
-        // handleValidation('main-container','none');
+    }
+    //Otherwise we can get datafrom here
+    else{
+        //if no result fount are here , then make empty/none
         handleValidation('no-result-found','none');
-
+        // Spinner Show 
+        handleValidation('spinner','block');
+        //if here are empty error , then make empty/none
+        handleValidation('empty-search-field','none');
+        //Get API access from here
         const url = `https://openlibrary.org/search.json?q=${inputText}`;
         fetch(url)
         .then(res => res.json())
         .then(data => displayBooks(data))
     }
 }
+// Validation function [Very usefull, Love it]
 const handleValidation = (id,display) => {
     const fieldId = document.getElementById(id);
     fieldId.style.display = display;
 }
+//When input field through a value for makeing the process and disply value
 const displayBooks = data => {
     const resultFound = data.numFound;
     const books = data.docs;
+    //If input value is not available in the API
     if(books.length === 0){
         handleValidation('search-result-found','none');
         handleValidation('no-result-found','block');
         handleValidation('empty-search-field','none');
+        handleValidation('spinner','none');
     }
+    //If input value are available in the API
     else{
         const mainContainer = document.getElementById('main-container');
-        // handleValidation('main-container','block');
         handleValidation('search-result-found','block');
+        //result founded value display  here
         const h4 = document.getElementById('search-result-found');
         h4.innerText = `Resulted founded: ${resultFound}`;
         mainContainer.appendChild(h4);
-
+        //Display all the founded value from API
         const bookContainer = document.getElementById('books-container');
         bookContainer.textContent = '';
         books?.forEach(book => {
@@ -51,15 +64,16 @@ const displayBooks = data => {
                 <div class="card my-1">
                     <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top img-fluid" alt="..." style="height: 250px">
                     <div class="card-body">
-                        <h5 class="card-title">${book.title}</h5>
+                        <h5 class="card-title">Book name: ${book.title}</h5>
                         <p><b>Author:</b> ${book.author_name ? book.author_name[0] : 'Unknown'}</p>
                         <p><b>Publisher:</b> ${book.publisher ? book.publisher[0] : 'Unknown'}</p>
                         <p><b>First Publish:</b> ${book.first_publish_year ? book.first_publish_year : 'Unknown'}</p>
-                        
                     </div>
                 </div> 
             `;
             bookContainer.appendChild(div);
         });
+        //make the spinner off after loading data
+        handleValidation('spinner','none');
     }
 }
